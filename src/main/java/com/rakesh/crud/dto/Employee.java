1eb99a444eac2508ec8a2a7616b245ba2962f723
Rakesh Gowda <rakeshgowda3101@gmail.com>
2023-07-04 15:48:50 +0530

    audit imple code

--- src/main/java/com/rakesh/crud/dto/Employee.java
+++ src/main/java/com/rakesh/crud/dto/Employee.java
@@ -1,22 +1,32 @@
 package com.rakesh.crud.dto;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.envers.AuditOverride;
+import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.rakesh.crud.audit.Auditable;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "employees")
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employee {
+@EntityListeners(AuditingEntityListener.class)
+public class Employee extends Auditable {
 
 	@Id
     @GeneratedValue
